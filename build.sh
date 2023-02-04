e4eb4f1ecba73fe1005d4857798b6b1aea8dd433
Thezo1 <rasgenes@gmail.com>
2023-02-04 17:32:07 +0100

    audio square wave

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -xe
+
+mkdir -p ./build
+pushd ./build
+gcc ~/Documents/code/c/uglykidjoe/code/sdl_uglykidjoe.c -o uglykidjoe `sdl2-config --cflags --libs`
+
+./uglykidjoe
+
+popd
--- code/sdl_uglykidjoe.c
+++ code/sdl_uglykidjoe.c
@@ -33,51 +33,41 @@ typedef struct SDL_WindowDimensionResult
     int heigth;
 }SDL_WindowDimensionResult;
 
+typedef struct RingBuffer
+{
+    int size;
+    int write_cursor;
+    int play_cursor;
+    void* data;
+}RingBuffer;
+
 global_variable SDL_OffScreenBuffer global_back_buffer;
 SDL_GameController *controller_handles[MAX_CONTROLLERS];
 SDL_Haptic *rumble_handles[MAX_CONTROLLERS];
+RingBuffer audio_ring_buffer;
 
 bool handle_event(SDL_Event *event);
 static void SDL_ResizeTexture(SDL_OffScreenBuffer *buffer, SDL_Renderer *renderer, int width, int height);
 static void SDL_UpdateWindow(SDL_OffScreenBuffer buffer, SDL_Window *Window, SDL_Renderer *renderer);
 static void render_gradient(SDL_OffScreenBuffer *buffer, int blue_offset, int green_offset);
 static SDL_WindowDimensionResult SDL_GetWindowDimension(SDL_Window *window);
+static void SDLAudioCallback(void *user_data, uint8 *audio_data, int length);
+static void SDL_OpenGameController();
+static void SDL_CloseCotroller();
+static void SDL_InitAudio(int32 samples_per_second, int32 buffer_size);
 
 int main(int argc, char *argv[])
 {
     SDL_Window *window;
     SDL_Renderer *renderer;
 
-    if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER | SDL_INIT_HAPTIC) != 0)
+    if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER | SDL_INIT_HAPTIC | SDL_INIT_AUDIO) != 0)
     {
         // TODO:(zourt) SDL did not init
     }
 
-    //TODO:(zourt) put into a function
-    int max_joysticks = SDL_NumJoysticks();
-    int controller_index = 0;
-    for(int joystick_index = 0;joystick_index<max_joysticks;joystick_index++)
-    {
-        if(!SDL_IsGameController(joystick_index))
-        {
-            continue;
-        }
-
-        if(controller_index >= MAX_CONTROLLERS)
-        {
-            break;
-        }
-        controller_handles[controller_index] = SDL_GameControllerOpen(joystick_index);
-        rumble_handles[controller_index] = SDL_HapticOpen(joystick_index);
-
-        if(rumble_handles[controller_index] && SDL_HapticRumbleInit(rumble_handles[controller_index]) != 0 )
-        {
-            SDL_HapticClose(rumble_handles[controller_index]);
-            rumble_handles[controller_index] = 0;
-        }
-
-        controller_index ++;
-    }
+    // Initialise game controllers
+    SDL_OpenGameController();
 
     window = SDL_CreateWindow("uglykidjoe", 
                               SDL_WINDOWPOS_UNDEFINED, 
@@ -101,6 +91,20 @@ int main(int argc, char *argv[])
     result = SDL_GetWindowDimension(window);
     SDL_ResizeTexture(&global_back_buffer, renderer, result.width, result.heigth);
 
+    // NOTE: Test sound
+    int samples_per_second = 48000;
+    int tone_hz = 256;
+    int16 tone_volume = 3000;
+    uint32 running_sample_index = 0;
+    int square_wave_period = samples_per_second / tone_hz;
+    int half_square_wave_period = square_wave_period/ 2;
+    int bytes_per_sample = sizeof(int16) * 2;
+    int secondary_buffer_size = samples_per_second * bytes_per_sample;
+
+    // Open audio device
+    SDL_InitAudio(48000, secondary_buffer_size);
+    bool sound_is_playing = false;
+
     int x_offset = 0;
     int y_offset = 0;
     while(running)
@@ -111,10 +115,6 @@ int main(int argc, char *argv[])
         {
             running = false;
         }
-        render_gradient(&global_back_buffer, x_offset, y_offset);
-        SDL_UpdateWindow(global_back_buffer, window, renderer);
-
-        ++x_offset;
 
         for(int controller_index = 0;controller_index<MAX_CONTROLLERS;controller_index++)
         {
@@ -156,16 +156,67 @@ int main(int argc, char *argv[])
             }
         }
 
+        render_gradient(&global_back_buffer, x_offset, y_offset);
+
+        SDL_LockAudio();
+        int byte_to_lock = running_sample_index*bytes_per_sample % secondary_buffer_size;
+        int bytes_to_write;
+        if(byte_to_lock == audio_ring_buffer.play_cursor)
+        {
+            bytes_to_write = secondary_buffer_size;
+        }
+        else if(byte_to_lock > audio_ring_buffer.play_cursor)
+        {
+            bytes_to_write = (secondary_buffer_size - byte_to_lock);
+            bytes_to_write += audio_ring_buffer.play_cursor;
+        }
+        else
+        {
+            bytes_to_write = audio_ring_buffer.play_cursor - byte_to_lock;
+        }
+
+        void *region1 = (uint8*)audio_ring_buffer.data + byte_to_lock;
+        int region1_size = bytes_to_write;
+        if (region1_size + byte_to_lock > secondary_buffer_size) region1_size = secondary_buffer_size - byte_to_lock;
+        void *region2 = audio_ring_buffer.data;
+        int region2_size = bytes_to_write - region1_size;
+        SDL_UnlockAudio();
+        int region1_sample_count = region1_size/bytes_per_sample;
+        int16 *sample_out = (int16 *)region1;
+        for(int sample_index = 0;
+            sample_index < region1_sample_count;
+            ++sample_index)
+        {
+            int16 sample_value = ((running_sample_index++ / half_square_wave_period) % 2) ? tone_volume : -tone_volume;
+            *sample_out++ = sample_value;
+            *sample_out++ = sample_value;
+        }
+
+        int region2_sample_count = region2_size/bytes_per_sample;
+        sample_out = (int16 *)region2;
+        for(int sample_index = 0;
+            sample_index < region2_sample_count;
+            ++sample_index)
+        {
+            int16 sample_value = ((running_sample_index++ / half_square_wave_period) % 2) ? tone_volume : -tone_volume;
+            *sample_out++ = sample_value;
+            *sample_out++ = sample_value;
+        }
+
+
+        if(!sound_is_playing)
+        {
+            SDL_PauseAudio(0);
+            sound_is_playing = true;
+        }
+
+
+        SDL_UpdateWindow(global_back_buffer, window, renderer);
+        ++x_offset;
+
     }
 
-    // SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "ugly kid joe", "This is Ugly Kid Joe", 0);
-    for(int controller_index = 0;controller_index<MAX_CONTROLLERS;controller_index++)
-    {
-        if(controller_handles[controller_index])
-        {
-            SDL_GameControllerClose(controller_handles[controller_index]);
-        }
-    }
+    SDL_CloseCotroller();
     SDL_Quit();
     printf("Hello Something\n");
     return 0;
@@ -200,7 +251,14 @@ bool handle_event(SDL_Event *event)
                 was_down = true;
             }
 
-            if(event->key.repeat == 1)
+            bool alt_key_was_down = (event->key.keysym.mod & KMOD_ALT);
+
+            if(keycode == SDLK_F4 && alt_key_was_down)
+            {
+                should_quit = true;
+            }
+
+            if(event->key.repeat == 0)
             {
                 if(keycode == SDLK_w)
                 {
@@ -278,7 +336,6 @@ bool handle_event(SDL_Event *event)
 
     return should_quit;
 }
-
 
 static void SDL_ResizeTexture(SDL_OffScreenBuffer *buffer, SDL_Renderer *renderer, int width, int height)
 {
@@ -367,4 +424,89 @@ static SDL_WindowDimensionResult SDL_GetWindowDimension(SDL_Window *window)
     SDL_GetWindowSize(window, &result.width, &result.heigth);
 
     return result;
+}
+
+static void SDL_OpenGameController()
+{
+    int max_joysticks = SDL_NumJoysticks();
+    int controller_index = 0;
+    for(int joystick_index = 0;joystick_index<max_joysticks;joystick_index++)
+    {
+        if(!SDL_IsGameController(joystick_index))
+        {
+            continue;
+        }
+
+        if(controller_index >= MAX_CONTROLLERS)
+        {
+            break;
+        }
+        controller_handles[controller_index] = SDL_GameControllerOpen(joystick_index);
+        rumble_handles[controller_index] = SDL_HapticOpen(joystick_index);
+
+        if(rumble_handles[controller_index] && SDL_HapticRumbleInit(rumble_handles[controller_index]) != 0 )
+        {
+            SDL_HapticClose(rumble_handles[controller_index]);
+            rumble_handles[controller_index] = 0;
+        }
+
+        controller_index ++;
+    }
+}
+
+static void SDLAudioCallback(void *user_data, uint8 *audio_data, int length)
+{
+    RingBuffer *audio_ring_buffer = (RingBuffer *)user_data;
+    int region1_size = length;
+    int region2_size = 0;
+    if(audio_ring_buffer->play_cursor + length > audio_ring_buffer->size)
+    {
+        region1_size = audio_ring_buffer->size - audio_ring_buffer->play_cursor;
+        region2_size = length - region1_size;
+    }
+
+    memcpy(audio_data, (uint8 *)(audio_ring_buffer->data) + audio_ring_buffer->play_cursor, region1_size);
+    memcpy(&audio_data[region1_size], audio_ring_buffer->data, region2_size);
+
+    // NOTE:(zourt) 2048 is the size of sdl's audio buffer in bytes
+    audio_ring_buffer->play_cursor = (audio_ring_buffer->play_cursor + length) % audio_ring_buffer->size;
+    audio_ring_buffer->write_cursor = (audio_ring_buffer->play_cursor + 2048) % audio_ring_buffer->size;
+}
+
+static void SDL_InitAudio(int32 samples_per_second, int32 buffer_size)
+{
+    SDL_AudioSpec audio_settings = {0};
+    audio_settings.freq = samples_per_second;
+    audio_settings.format = AUDIO_S16LSB;
+    audio_settings.channels = 2;
+    audio_settings.samples = buffer_size;
+    audio_settings.callback = &SDLAudioCallback;
+    audio_settings.userdata = &audio_ring_buffer;
+
+    audio_ring_buffer.size = buffer_size;
+    audio_ring_buffer.data = malloc(buffer_size);
+    audio_ring_buffer.play_cursor = audio_ring_buffer.write_cursor = 0;
+
+    SDL_OpenAudio(&audio_settings, 0);
+    printf("Initialised an Audio device at frequency %d Hz, %d Channels\n",
+           audio_settings.freq, audio_settings.channels);
+
+    if(audio_settings.format != AUDIO_S16LSB)
+    {
+        printf("Wrong audio format\n");
+        SDL_CloseAudio();
+    }
+
+    SDL_PauseAudio(0);
+}
+
+static void SDL_CloseCotroller()
+{
+    for(int controller_index = 0;controller_index<MAX_CONTROLLERS;controller_index++)
+    {
+        if(controller_handles[controller_index])
+        {
+            SDL_GameControllerClose(controller_handles[controller_index]);
+        }
+    }
 }
